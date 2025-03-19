d3418fd3fb9115e87ac835258b1342e9adaf98f7
Asifur Chowdhury <asifurchowdhury@Asifurs-MacBook-Pro.local>
2025-03-20 00:39:11 +1100

    Initial commit

---
+++ src/app/funds/page.tsx
@@ -0,0 +1,9 @@
+export default function Owners() {
+    return (
+      <main className="p-6">
+        <h1 className="text-3xl font-bold">Owners Dashboard</h1>
+        <p className="mt-4">Here, you can manage information about the source of funds and the monthly expenditure.</p>
+      </main>
+    );
+  }
+  
