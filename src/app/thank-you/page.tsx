5120171e0d46662f038cc76829bcd9b5f0308a94
acho0821 <acho0821@uni.sydney.edu.au>
2025-03-23 00:54:52 +1100

    Actually add thank-you page to repo

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -3,6 +3,8 @@ export default function ThankYouPage() {
       <div style={{ padding: "2rem" }}>
         <h1>Thank you!</h1>
         <p>Your maintenance issue has been submitted successfully!</p>
+        <p>We'll follow up soon!</p>
+
       </div>
     );
   }
